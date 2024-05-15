7c38f292cf0baea2ece3bdc794856cdd685690fb
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2024-05-15 12:11:11 +0300

    commit

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 function detectUserDevice() {
     var userAgent = navigator.userAgent;
     if (/huawei/i.test(userAgent)) {
-        return "Huawei";
+        // return "Huawei";
+        window.location.href = "https://appgallery8.huawei.com/#/app/C100847609"
     }
     if (/android/i.test(userAgent)) {
-        return "Android";
+        // return "Android";
+        window.location.href = "https://play.google.com/store/apps/details?id=ru.ozon.app.android&amp;hl=ru";
     }
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return "Apple";
+        // return "Apple";
+        window.location.href = "https://itunes.apple.com/us/app/id407804998"
     }
     if (/Win32|Win64|Windows|WinCE/.test(userAgent)) {
-        return "Windows";
+        // return "Windows";
     }
     if (/Mac/.test(userAgent)) {
-        return "Mac OS";
+        // return "Mac OS";
     }
 
 }
-const device = detectUserDevice()
+detectUserDevice();
+// const device = detectUserDevice()
 
-document.getElementById('title').textContent = `Привет! Вы зашли с устройства ${device}`
+// document.getElementById('title').textContent = `Привет! Вы зашли с устройства ${device}`
